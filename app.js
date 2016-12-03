073f1038d7e70b106df1d9d6a64475501fa8e7ea
ybinstock <ybinstock@gmail.com>
2016-12-03 17:28:45 -0500

    next

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+
+//For eyes
+  function toggleEyes() {
+    document.getElementById('instructions').setAttribute('visible', 'false');
+  }
+
+  setTimeout(toggleEyes, 4000);
+
+});
